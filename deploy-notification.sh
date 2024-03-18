ff562dfa0996140f6d3954420e10039137019b29
Nithin K Joy <nithinjoy121212@gmail.com>
2024-03-18 19:31:09 +0530

    added deploy notification script

---
+++ deploy-notification.sh
@@ -0,0 +1,21 @@
+if [ $? -eq 0 ]; then
+
+# Define the notification title
+title="Theme Deployed Successfully"
+
+# Use osascript to trigger the notification with only the title
+osascript -e "display notification \"\" with title \"$title\" sound name \"default\""
+
+# Use the say command with a specified voice for voice notification
+say -v Samantha "Theme deployed successfully"
+else
+
+# Define the notification title
+title="Deployment failed"
+
+# Use osascript to trigger the notification with only the title
+osascript -e "display notification \"\" with title \"$title\" sound name \"default\""
+
+# Use the say command with a specified voice for voice notification
+say -v Samantha "Deployment Failed"
+fi
